25e4df4155e3a9ccf3090a093cd2d9b41f074d2a
Jose Carlos Montero Gomez <sipox11@gmail.com>
2015-07-12 23:45:16 +0000

    Improve search capabilities with keywords

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,9 @@ class Book < ActiveRecord::Base
   
   scope :finished, -> { where.not(finished_on: nil) }
   scope :recent, -> { where('finished_on > ?', 1.year.ago) }
-  scope :search, ->(keyword) { where(title: keyword) if keyword.present? }
+  scope :search, ->(keyword) { where('keywords LIKE ?', "%#{keyword.downcase}%")}
+  
+  before_save :set_keywords
   
   # def self.recent
   #   where('finished_on > ?', 1.year.ago)
@@ -19,4 +21,9 @@ class Book < ActiveRecord::Base
   def finished?
     finished_on.present?
   end
+  
+  protected
+    def set_keywords
+      self.keywords = [title, author, description].map(&:downcase).join(' ')
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712223234) do
+ActiveRecord::Schema.define(version: 20150712233700) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150712223234) do
     t.datetime "updated_at",  null: false
     t.integer  "rating"
     t.date     "finished_on"
+    t.text     "keywords"
   end
 
 end
