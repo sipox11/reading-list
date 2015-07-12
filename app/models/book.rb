8524f6ec0bae5b5900a2fa7dcf7809a7695348a4
Jose Carlos Montero Gomez <sipox11@gmail.com>
2015-07-12 22:59:42 +0000

    Add finished and recent scopes to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ActiveRecord::Base
+  
+  scope :finished, -> { where.not(finished_on: nil) }
+  # scope :recent, -> { where('finished_on > ?', 1.year.ago) }
+  
+  def self.recent
+    where('finished_on > ?', 1.year.ago)
+  end
+  
   def finished?
     finished_on.present?
   end
