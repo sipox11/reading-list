5f9d54d1377132bad80b517858e6d8fcb26add7c
Jose Carlos Montero Gomez <sipox11@gmail.com>
2015-07-12 23:34:11 +0000

    Initial search. Title only.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,20 @@
 class Book < ActiveRecord::Base
   
   scope :finished, -> { where.not(finished_on: nil) }
-  # scope :recent, -> { where('finished_on > ?', 1.year.ago) }
+  scope :recent, -> { where('finished_on > ?', 1.year.ago) }
+  scope :search, ->(keyword) { where(title: keyword) if keyword.present? }
   
-  def self.recent
-    where('finished_on > ?', 1.year.ago)
-  end
+  # def self.recent
+  #   where('finished_on > ?', 1.year.ago)
+  # end
+  
+  # def self.search(keyword)
+  #   if keyword.present?
+  #     where(title: keyword) 
+  #   else
+  #     all
+  #   end
+  # end
   
   def finished?
     finished_on.present?
